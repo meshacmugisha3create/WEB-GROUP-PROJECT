e7ebd51723e11103fd8ce3cde0bd7cffe6fc265b
mulyantimark <marcmulyantiat@gmail.com>
2026-04-09 10:19:57 +0300

    Members look page2.jsx

--- FRONTEND/Student Study Group Finder/src/App.jsx
+++ FRONTEND/Student Study Group Finder/src/App.jsx
@@ -1,9 +1,14 @@
- import Page1 from "./Page1";
- function App() {
-    return (
-        <div>
-            <Page1 />
-        </div>
-    );
-}   
+import React from "react";
+import Page2 from "./page2"; 
+
+function App() {
+  return (
+    <div className="App">
+      {/* Rendering Page2 to view the Group Creation Form for Milestone Two */}
+      <Page2 />
+    </div>
+  );
+}
+
 export default App;
+
